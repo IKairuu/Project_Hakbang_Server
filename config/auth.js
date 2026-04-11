58aa14045033b7e9ca51ab4b304ee48bfec11760
Kairu <abandiakyle2112@gmail.com>
2026-04-11 18:12:09 +0800

    Developed token based JWT authentication for users

--- config/auth.js
+++ config/auth.js
@@ -1,3 +1,29 @@
-import express from "express" ;
+import jwt from "jsonwebtoken" ;
+import * as dotenv from "dotenv" ;
+dotenv.config() ;
 
-const authRouter = express.Router() ;
+
+export function authentication(req, res, next) {
+        const header = req.headers.authorization ;
+        console.log(header) ;
+        if (!header)
+        {
+            return res.status(401).json({message: "There is no token"}) ;
+        }
+
+        const token = header.split(" ")[1] ;
+        if (!token)
+            return res.status(401).json({message: "Invalid token format"}) ; 
+            
+        jwt.verify(token, process.env.JWT_SECRET_KEY, (error, user) => {
+            if (error)
+            {
+                return res.status(403).json({message: "Invalid token"}) ;
+            }
+            req.user = user ;
+            next() ;
+        }) ;
+        
+    } ;
+
+
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,9 @@
 import express from "express";
 import {addUserData, getUserData} from "../database/database.js";
+import {authentication} from "../config/auth.js" ;
+import jwt from "jsonwebtoken" ;
+import * as dotenv from "dotenv" ;
+dotenv.config() ;
 
 const user = express.Router() ;
 
@@ -13,7 +17,14 @@ user.post("/login", async (req, res) => {
     let user_data = await getUserData(req.body) ;
     if (user_data == null)
         return res.status(401).json({message:"Invalid Username or password"}) ;
-    return res.status(200).json(user_data) ;
+
+    let accessToken = jwt.sign({data: user_data}, process.env.JWT_SECRET_KEY) ;
+    return res.status(200).json({message: "User Successfully logged in", token: accessToken}) ;
+}) ;
+
+//FOR TESTING PURPOSES
+user.get("/auth/test-message",authentication,(req, res) => {
+    return res.status(200).json({message:"Successful authentication"}) ;
 }) ;
 
 export default user ;
