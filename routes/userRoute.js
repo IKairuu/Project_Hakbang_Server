ca08c94c6f1ff6b32c3b4b86612c7b41f889881e
Kairu <abandiakyle2112@gmail.com>
2026-04-17 20:18:38 +0800

    Added user data into returned json from login endpoint

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -17,7 +17,7 @@ user.post("/signup", async (req, res) =>
 user.post("/login", async (req, res) => {
     let user_data = await getUserData(req.body) ;
     if (user_data == null)
-        return res.status(401).json({message:"Invalid Username or password", "status": 401}) ;
+        return res.status(401).json({message:"Invalid Username or password", "status": 401, "data": user_data}) ;
 
     let accessToken = jwt.sign({data: user_data}, process.env.JWT_SECRET_KEY) ;
     return res.status(200).json({message: "User Successfully logged in", token: accessToken, "status": 200}) ;
