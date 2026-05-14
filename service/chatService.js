32b9cbe44448c1041db5d33dd9ffc5f09f567e71
Kairu <abandiakyle2112@gmail.com>
2026-05-14 13:59:04 +0800

    Chat error message

--- service/chatService.js
+++ service/chatService.js
@@ -41,6 +41,6 @@ export const message_gabay = async (message, email) =>
     }
     catch (error)
     {
-        throw new Error(`Chat Error: ${error.message}`) ;
+        throw new Error(`Chat Error: ${error.message["error"]["message"]}`) ;
     }
 }
