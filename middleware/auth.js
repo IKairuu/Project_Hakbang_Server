c6cf74d8cb01ef08104fed632db04b56c8e17271
Kairu <abandiakyle2112@gmail.com>
2026-05-15 15:59:52 +0800

    Added error details in auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,17 +7,17 @@ export function authentication(req, res, next) {
         const header = req.headers.authorization ;
         if (!header)
         {
-            return res.status(402).json({message: "There is no token", status: 402}) ;
+            return res.status(400).json({message: "Token Error: There is no token", status: 402}) ;
         }
 
         const token = header.split(" ")[1] ;
         if (!token)
-            return res.status(403).json({message: "Invalid token format", status : 403}) ; 
+            return res.status(400).json({message: "Token Error: Invalid token format", status : 400}) ; 
             
         jwt.verify(token, process.env.JWT_SECRET_KEY, (error, user) => {
             if (error)
             {
-                return res.status(404).json({message: "Invalid token", status : 404}) ;
+                return res.status(400).json({message: "Token Error: Invalid token", status : 400}) ;
             }
             req.user = user ;
             next() ;
