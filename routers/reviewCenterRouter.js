ce960993d8c6ad340be82c1e7326fcc5b3662371
Kairu <abandiakyle2112@gmail.com>
2026-05-16 10:09:25 +0800

    Deletion: Removed unnecessary codes

--- routers/reviewCenterRouter.js
+++ routers/reviewCenterRouter.js
@@ -1,6 +1,4 @@
 import express from "express" ;
-import { authentication } from "../middleware/auth.js";
-import { getHubs } from "../database/database.js" ;
 import { centerList } from "../controller/reviewCenterController.js";
 
 const center = express.Router() ;
