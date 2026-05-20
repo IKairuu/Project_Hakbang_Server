deaf41a6f01699eb2a3e7af76ae326ea2f54398a
Kairu <abandiakyle2112@gmail.com>
2026-05-20 16:49:52 +0800

    Fix: added port 465  and set secure as  true, and  changed service into smtp.gmail.com for smtp connection from render into gmail

--- config/mailer_config.js
+++ config/mailer_config.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer" ;
 import * as dotenv from "dotenv" ;
 dotenv.config() ;
 
-export const transporter = nodemailer.createTransport({service: "gmail", auth: {
+export const transporter = nodemailer.createTransport({service: "smtp.gmail.com", port: 465, secure: true, auth: {
     user:process.env.GMAIL_USER,
     pass:process.env.GMAIL_PASSWORD
-}})
+}, connectionTimeout: 10000})
