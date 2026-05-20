f94c5c4f18060c8105be5845683025963be854b1
Kairu <abandiakyle2112@gmail.com>
2026-05-20 16:59:36 +0800

    Fix: Force dns ipv4 instead of ipv6

--- config/mailer_config.js
+++ config/mailer_config.js
@@ -1,7 +1,9 @@
 import nodemailer from "nodemailer" ;
 import * as dotenv from "dotenv" ;
+import dns from "dns" ;
 dotenv.config() ;
 
+dns.setDefaultResultOrder("ipv4first");
 export const transporter = nodemailer.createTransport({host:"smtp.gmail.com", port: 465, secure: true, auth: {
     user:process.env.GMAIL_USER,
     pass:process.env.GMAIL_PASSWORD
